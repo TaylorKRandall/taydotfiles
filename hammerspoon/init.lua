ac0ef88b41e65457a5b74c261c81d7bc28b1408f
Taylor Randall <taylor.k.randall@gmail.com>
2020-02-19 15:43:47 -0800

    added variables for office monitors

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -5,6 +5,10 @@
 	comment
 --]]
 
+local laptopScreen = "Color LCD"
+local portraitScreen = "Dell P2211H"
+local screen2 = "Dell 2407WFP"
+
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
 	hs.notify.new({title="Hammerspoon", informativeText="hello woRld"}):send()
 end)
